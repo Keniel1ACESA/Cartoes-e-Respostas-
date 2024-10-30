c889e85ad6a0f7cc74c7eb1b71404041b946a9bf
Keniel1ACESA <144136104+Keniel1ACESA@users.noreply.github.com>
2024-10-29 21:19:32 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,68 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Capitais',
+    'Qual a cipital do estado de Santa Catarina?',
+    'Florianópolis'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Capitais',
+    'Qual a cipital do estado Paraná?',
+    'Curitiba'
+
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+      'Capitais',
+    'Qual a cipital do estado São Paulo?',
+    'São Paulo'
+
 )
+
+criaCartao(
+      'Capitais',
+    'Qual a cipital do estado de Minas Gerais?',
+    'Belo Horizonte'
+
+)
+
+criaCartao(
+      'Capitais',
+    'Qual a cipital do estado de Mato Grosso do Sul?',
+    'Campo Grande'
+
+)
+
+criaCartao(
+      'Capitais',
+    'Qual a cipital do estado de Rio de Janeiro?',
+    'Rio de Janeiro'
+
+)
+
+criaCartao(
+      'Capitais',
+    'Qual a cipital do estado de Espirto Santo?',
+    'Vitória'
+
+)
+
+criaCartao(
+      'Capitais',
+    'Qual a cipital do estado de Goíás?',
+    'Goiânia'
+
+)
+
+criaCartao(
+      'Capitais',
+    'Qual a cipital do estado de Moto Grosso?',
+    'Cuiabá'
+
+)
+
+criaCartao(
+      'Capitais',
+    'Qual a cipital do estado de Rondônia?',
+    'Porto Velho'
+
+
